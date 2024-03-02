dfa3ca1d449cf26e49c37e8c19205a22a90d9732
minglani <khushwant@hotmail.co.uk>
2024-03-02 17:03:45 +0000

    modified:   EKS/main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -8,6 +8,7 @@ module "vpc" {
 
   private_subnets      = var.private_subnets
   public_subnets       = var.public_subnets
+  
   enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
